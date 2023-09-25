06a05c2191ca921326a81c85690be9c358b81e86
ManuelGuaicaraDagger <manuelgd1997@gmail.com>
2023-09-25 09:52:53 -0300

    M03

--- M03 JavaScript Fundamentos/Ejercicio 04.js
+++ M03 JavaScript Fundamentos/Ejercicio 04.js
@@ -4,41 +4,53 @@
 // ⛔️ Recuerda que debes utilizar el objeto global "Math".
 
 function elevarAlCuadrado(num) {
-   // Retorna el valor de "num" elevado al cuadrado.
-   // Tu código:
+  // Retorna el valor de "num" elevado al cuadrado.
+  // Tu código:
+  let cuadrado = Math.pow(num, 2);
+  return cuadrado;
 }
 
 function elevarAlCubo(num) {
-   // Retorna el valor de "num" elevado al cubo.
-   // Tu código:
+  // Retorna el valor de "num" elevado al cubo.
+  // Tu código:
+  let cuadrado = Math.pow(num, 3);
+  return cuadrado;
 }
 
 function elevar(num, exponent) {
-   // Retorna el valor de "num" elevado al exponente "exponent".
-   // Tu código:
+  // Retorna el valor de "num" elevado al exponente "exponent".
+  // Tu código:
+  let cuadrado = Math.pow(num, exponent);
+  return cuadrado;
 }
 
 function redondearNumero(num) {
-   // Redondea "num" al entero más próximo y retórnalo.
-   // Tu código:
+  // Redondea "num" al entero más próximo y retórnalo.
+  // Tu código:
+  let entero = Math.round(num);
+  return entero;
 }
 
 function redondearHaciaArriba(num) {
-   // Redondea "num" hacia arriba y retórnalo.
-   // Tu código:
+  // Redondea "num" hacia arriba y retórnalo.
+  // Tu código:
+  let entero = Math.ceil(num);
+  return entero;
 }
 
 function numeroRandom() {
-   // Genera un número al azar entre 0 y 1 y retórnalo.
-   // Tu código:
+  // Genera un número al azar entre 0 y 1 y retórnalo.
+  // Tu código:
+  let random = Math.random(0, 1);
+  return random;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
-   elevarAlCuadrado,
-   elevarAlCubo,
-   elevar,
-   redondearNumero,
-   redondearHaciaArriba,
-   numeroRandom,
+  elevarAlCuadrado,
+  elevarAlCubo,
+  elevar,
+  redondearNumero,
+  redondearHaciaArriba,
+  numeroRandom,
 };
